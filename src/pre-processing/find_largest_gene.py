e8d0d4a696256b0c60d2aea6f5adb7613be452d1
matiastba <33896722+matiastba@users.noreply.github.com>
2019-11-04 16:14:21 +0100

    function for removing small genes added. Function for finding largest gene added.

--- src/pre-processing/find_largest_gene.py
+++ src/pre-processing/find_largest_gene.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov  4 15:10:29 2019
+Created on Mon Nov  4 15:51:35 2019
 
 @author: mfba
 """
@@ -8,9 +8,9 @@ Created on Mon Nov  4 15:10:29 2019
 import pandas as pd 
 import os 
 
-def remove_small_genes(annotations, min_gene_size):
-        
-    genes_to_remove = []
+def find_largest_gene(annotations):
+    
+    largest_gene = 0
     
     # Iterate over rows
     for index, row in annotations.iterrows():
@@ -19,12 +19,10 @@ def remove_small_genes(annotations, min_gene_size):
         gene_length = row['end']-row['start']
         
         # Check if smaller than minimum length
-        if gene_length < min_gene_size:
-            genes_to_remove.append(index)
-
-    # Remove all genes smaller than minimum
-    annotations = annotations.drop(annotations.index[genes_to_remove])    
-    return annotations
+        if gene_length > largest_gene:
+            largest_gene = gene_length
+            
+    return largest_gene
 
 
 if __name__ == '__main__':
@@ -37,10 +35,9 @@ if __name__ == '__main__':
     # Save gene meta data in Panda frame
     annotations = pd.read_csv(filepath,  sep='\t')
     
-    # Minimum gene size. All genes less than this will be removed.
-    min_gene_size = 200
-    
     # Retrieve valid genes
-    ann = remove_small_genes(annotations, min_gene_size)
+    largest_gene = find_largest_gene(annotations)
+    
+    print('Largest gene: ' + str(largest_gene))
     
     print('End of script.')
