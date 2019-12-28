6d96e804e6dc453bdc4004970d949207d2d3dc82
carrascomj <carrascomurielj@gmail.com>
2019-12-28 19:47:44 +0100

    feat: add arg options to preprocessing pipelines

--- src/pre-processing/preproc_pipeline.py
+++ src/pre-processing/preproc_pipeline.py
@@ -1,7 +1,8 @@
-# High-level functions that join all the pre-processing routines into a pipeline
+"""Join all the pre-processing routines into a pipeline."""
 
 import pandas as pd
 import pyfastx
+import re
 
 from build_dataset import build_padded_seqs, map_full_genomes, classify_genes
 from get_labeled_genes import (
@@ -15,6 +16,18 @@ from get_labeled_genes import (
 )
 
 
+def remove_unk(df, seq_col="sequence", known=None):
+    """Remove sequences with unknown characters."""
+    if not known:
+        # default to DNA
+        known = {"A", "C", "G", "T"}
+    # under the hood, pandas is calling re.search, so the pattern is compiled
+    # here to speed up the process
+    pat_known = re.compile(rf'[^{"".join(known)}]')
+    df_filtered = df[~df[seq_col].str.contains(pat_known)]
+    return df_filtered
+
+
 def pre_process(
     genome_file,
     feat_file,
@@ -23,7 +36,7 @@ def pre_process(
     sep="\t",
     out_file=False,
 ):
-    """ Master function
+    """Master function.
 
     Accepts a `genome_file` FASTA and a `feat_file` TSV and convers it to a pandas
     dataframe.
@@ -61,9 +74,14 @@ def pre_process(
     df_feat = pd.read_csv(feat_file, sep=sep)
     genome_length = len(genome)
 
-    # 2. STANDARDIZE DATA
-    # TODO: remove also big genes
-    df_feat = remove_small_genes(df_feat, min_gene_size)
+    # 2. STANDARIZE DATA
+    df_feat = df_feat[
+        df_feat.apply(
+            lambda x: x.end - x.start < max_gene_size
+            and x.end - x.start > min_gene_size,
+            axis=1,
+        )
+    ]
 
     # 3. GET RANGES OF FEATURES
     # Test genes
@@ -95,6 +113,8 @@ def window_pipeline(
     sep="\t",
     out_file=False,
     by=None,
+    manage_unk=True,
+    annotate=True,
 ):
     """Preprocessing pipeline that take equidistant windows in `feat_file`.
 
@@ -105,6 +125,16 @@ def window_pipeline(
         the first element corresponds to the first position in `genome_file`
         with respect to the real genome in `feat_file`. Only used when portions
         of a genome are provided.
+    manage_unk: bool
+        whether sequences with unknown characters - not one of A,G,C,T - should
+        be removed. These characters (N, S, etc.) are produced when, for
+        instanced, the experimental or assembly methods could not resolve a
+        position; i.e., there is uncertainty for the identity of this position.
+        If False, the sequences with these characters are kept. Default `True`.
+    annotate: bool
+        whether the fixed sequences that correspond to portions of genes should
+        be annotated with columns in the final dataset that provides
+        information about the gene function. Default `True`.
 
     Returns
     -------
@@ -153,7 +183,10 @@ def window_pipeline(
 
     # 4. BUILD THE PRE-PROCESSED DATAFRAME
     df_out = map_full_genomes(genome, positive, negative, partial)
-    df_out = classify_genes(df_out, df_feat, feature="gene")
+    if manage_unk:
+        df_out = remove_unk(df_out)
+    if annotate:
+        df_out = classify_genes(df_out, df_feat, feature="gene")
 
     return df_out
 
@@ -167,6 +200,7 @@ if __name__ == "__main__":
         f"\nRESULT OF TEST\n{len('RESULT OF TEST')*'='}\n{df.head(10)}\n"
         f"\ncolums -> {list(df.columns)}"
         f"\nlabels -> {pd.unique(df.label)}"
+        f"\nNumber of genes -> {(df.label=='gene').sum()}"
         f"\nNon indentified genes ranges -> {df.loc[df.label=='gene','name'].isna().sum()}"
     )
     assert not df.sequence.apply(lambda x: len(x) != 50).any()
